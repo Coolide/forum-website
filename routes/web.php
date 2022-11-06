58065085d06cdf5601c62144c1b6fa1afcc3723c
Coolide <khalid_evans@yahoo.com>
2022-11-06 21:20:18 +0000

    Added a new 'Staff' table

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return "This is home!";
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/welcome/{name}', function($name) {
+    return "Welcome back $name!";
+});
+
+Route::redirect('/here', 'there');
+
+Route::get('/there', function () {
+    return "Don't worry, you are there now!";
+});
 
 require __DIR__.'/auth.php';
