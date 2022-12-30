ee1443790365b36767429ed9b06485445f140f82
Coolide <khalid_evans@yahoo.com>
2022-12-30 02:21:24 +0000

    Added usernme to User table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('username')->unique();
             $table->string('country')->nullable();
             $table->string('email')->unique();
             $table->dateTime('date_of_birth')->nullable();
