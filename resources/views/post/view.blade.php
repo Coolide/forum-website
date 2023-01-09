352c5c8100eb7dd6aed51487f97be276b3461c05
Coolide <khalid_evans@yahoo.com>
2023-01-09 06:13:30 +0000

    added ajax to post comment items

--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -13,7 +13,11 @@
         .top-buffer { margin-top:20px; }
         .bottom-buffer {margin-bottom:20px;}
     </style>
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+        <meta name="csrf-token" content="{{ csrf_token() }}" /> 
+        
     <div class="container">
         <div class="row text-center">
             <h1><b>{{$post->title}}</b></h1>
@@ -60,20 +64,20 @@
             </div>
         @endif
         <div class="row bottom-buffer">
-            <form class="form-horizontal" method="POST" action="{{ route('comment.store') }}">
+            <form id="comment_form" class="form-horizontal" method="POST" action="{{route('comment.store')}}">
                 @csrf
                 @if(!empty(Auth::user()->username))
-                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                <input type="hidden" name="post_id" value="{{ $post->id }}">
+                <input type="hidden" name="user_id" id="ajax_user_id" value="{{ Auth::user()->id }}">
+                <input type="hidden" name="post_id" id="ajax_post_id" value="{{ $post->id }}">
                 <div class="form-group">
                     <label for="comment">Comment:</label>
-                    <textarea class="form-control" rows="1" id="description" name="description" value="{{ old('description') }}"></textarea>
+                    <textarea class="form-control" rows="1" id="ajax_description_id" name="description" value="{{ old('description') }}"></textarea>
                 </div>
-                <button type="submit" class="btn btn-default">Comment</button>
+                <button type="submit" class="btn btn-default ajax-button">Comment</button>
                 @else
                 <div class="form-group">
                     <label for="comment">Comment:</label>
-                    <textarea class="form-control disabled" rows="1" id="description" name="description" value="{{ old('description') }}"></textarea>
+                    <textarea class="form-control disabled" rows="1" id="ajax_description_id" name="description" value="{{ old('description') }}"></textarea>
                 </div>
                 <button type="submit" class="btn btn-default disabled">Comment</button>
                 @endif
@@ -123,4 +127,33 @@
         </div>
         @endforeach
     </div>
+    <script type="text/javascript">
+        $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+            $(".ajax-button").click(function(e){
+                e.preventDefault();
+
+                var user_id = $("#ajax_user_id").val();
+                var post_id = $("#ajax_post_id").val();
+                var description = $("#ajax_description_id").val();
+
+                $.ajax({
+                    url: "{{route('comment.store')}}",
+                    type:'POST',
+                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    data:{
+                        user_id:user_id,
+                        post_id:post_id,
+                        description:description
+                    }
+                });
+
+                $(document).ajaxStop(function(){
+                    window.location.reload();
+                });
+            });
+    </script>
 @endsection
